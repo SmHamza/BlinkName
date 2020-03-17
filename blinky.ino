938187697f5036aabf8cec1518d317960f0d8f46
SmHamza <52901691+SmHamza@users.noreply.github.com>
2020-03-17 16:55:50 +1100

    Add files via upload

---
+++ blinky.ino
@@ -0,0 +1,71 @@
+int led1 = D7;
+void setup() 
+{
+    pinMode (led1, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (3000);
+    
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (3000);
+    
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (3000);
+    
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (3000);
+    
+     digitalWrite(led1, HIGH);
+    delay (1000);
+    digitalWrite(led1, LOW);
+    delay (500);
+    digitalWrite(led1, HIGH);
+    delay (2000);
+    digitalWrite(led1, LOW);
+    delay (3000);
+    
+    
+}
